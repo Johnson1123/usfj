d7bec4abd31b04998ceff1f4b97deb568afbe0c4
Johnson1123 <onifadejohnson2017@gmail.com>
2025-04-25 00:31:07 -0500

    rectify routing

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,22 +1,22 @@
-import "./globals.css";
-import "../style/style.css";
-import ReduxProvider from "./provider";
+import './globals.css';
+import '../style/style.css';
+import ReduxProvider from './provider';
 
 export const metadata = {
-  title: "United Servants For Jesus",
-  description: "A Non-Governmental Organization",
+    title: 'United Servants For Jesus',
+    description: 'A Non-Governmental Organization',
 };
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <head>
-        <link rel="icon" href="/images/FavIcon.png" />
-      </head>
+    return (
+        <html lang="en">
+            <head>
+                <link rel="icon" href="/images/FavIcon.png" />
+            </head>
 
-      <body className={`font-manFont  antialiased`}>
-        <ReduxProvider>{children}</ReduxProvider>
-      </body>
-    </html>
-  );
+            <body className={`font-manFont  antialiased`}>
+                <ReduxProvider>{children}</ReduxProvider>
+            </body>
+        </html>
+    );
 }
--- src/app/shop/page.jsx
+++ src/app/shop/page.jsx
@@ -1,27 +1,27 @@
-"use client";
-import Header from "@/components/layout/Header";
-import ShopBanner from "@/components/medium/ShopBanner";
-import ShpCategories from "@/components/medium/ShpCategories";
-import StorySection from "@/components/shop/Story";
-import { TopProductsSection } from "@/components/shop/TopProductSlider";
-import InsightSection from "@/components/Homepage/InsightAndInpiration/Main";
-import React from "react";
-import Banner from "@/components/shop/Banner";
-import ReviewsSection from "@/components/shop/Reviews";
+'use client';
+import Header from '@/components/layout/Header';
+import ShopBanner from '@/components/medium/ShopBanner';
+import ShpCategories from '@/components/medium/ShpCategories';
+import StorySection from '@/components/shop/Story';
+import { TopProductsSection } from '@/components/shop/TopProductSlider';
+import InsightSection from '@/components/Homepage/InsightAndInpiration/Main';
+import React from 'react';
+import Banner from '@/components/shop/Banner';
+import ReviewsSection from '@/components/shop/Reviews';
 
-function page() {
-  return (
-    <>
-      <Header />
-      <ShopBanner />
-      <ShpCategories />
-      <TopProductsSection />
-      <StorySection />
-      <ReviewsSection />
-      <InsightSection />
-      <Banner />
-    </>
-  );
+function Page() {
+    return (
+        <>
+            <Header />
+            <ShopBanner />
+            <ShpCategories />
+            <TopProductsSection />
+            <StorySection />
+            <ReviewsSection />
+            <InsightSection />
+            <Banner />
+        </>
+    );
 }
 
 export default page;
