e73c06b46ab30f2af811908a9221ec5f3dd1386e
KaltechCode <kaltechconsultant@gmail.com>
2025-05-24 23:12:00 -0500

    Adding full order details to email

--- src/app/api/completed-webhook/route.js
+++ src/app/api/completed-webhook/route.js
@@ -2,94 +2,6 @@
 
 import sendEmail from '@/utils/email';
 import { headers } from 'next/headers';
-// import { buffer } from 'micro';
-// import Stripe from 'stripe';
-
-// // Ensure the key is kept out of any version control system you might be using.
-// const stripe = Stripe(process.env.STRIPE_KEY);
-
-// // This is your Stripe CLI webhook secret for testing your endpoint locally.
-// const endpointSecret = process.env.SESSION_COMPLETED;
-
-// export const config = {
-//     api: {
-//         bodyParser: false, // Need raw body for webhook signature verification
-//     },
-// };
-
-// export async function POST(request) {
-//     if (req.method === 'POST') {
-//         const buf = await buffer(request);
-//         const sig = request.headers['stripe-signature'];
-
-//         let event;
-
-//         try {
-//             event = stripe.webhooks.constructEvent(
-//                 buf.toString(),
-//                 sig,
-//                 endpointSecret,
-//             );
-//             // const email = event.data.object.metadata.email;
-//             // if (email) {
-
-//             // }
-//         } catch (err) {
-//             console.log(`Webhook Error: ${err.message}`);
-//             return Response.json({
-//                 success: false,
-//                 message: 'Failed to send email',
-//                 error: err.message,
-//             });
-//         }
-
-//         // Handle the event
-//         switch (event.type) {
-//             case 'checkout.session.completed':
-//                 const checkoutSessionCompleted = event.data.object;
-//                 // Then define and call a function to handle the event checkout.session.completed
-//                 console.log(
-//                     'Checkout Session Completed:',
-//                     checkoutSessionCompleted,
-//                 );
-
-//                 const subject = 'Checkout Session Completed';
-//                 const body = `Your checkout session has been completed successfully. Your order is being processed.`;
-//                 // Assuming there's a function to send emails
-//                 await sendEmail(
-//                     {
-//                         name: 'Kaltech Contact Form',
-//                         address: 'usfj_auth@kaltechconsultancy.tech',
-//                     },
-//                     { email: 'onifadejohnson2014@gmail.com', name: 'admin' },
-//                     'subject',
-//                     'Order complete',
-//                 );
-//                 // You might want to update your database, fulfill the order,
-//                 // send emails, etc., based on the session details.
-//                 break;
-//             // ... handle other event types
-//             default:
-//                 console.log(`Unhandled event type ${event.type}`);
-//         }
-
-//         // Return a 200 response to acknowledge receipt of the event
-//         return Response.json({
-//             success: true,
-//             message: 'good',
-//             error: 'Email failed',
-//         });
-//     } else {
-//         Response.setHeader('Allow', ['POST']);
-//         return Response.json({
-//             success: false,
-//             message: 'Failed to send email',
-//             error: 'Resquest method not allow',
-//         });
-//     }
-// }
-
-// pages/api/webhook/stripe.ts
 import Stripe from 'stripe';
 const stripe = new Stripe(process.env.STRIPE_KEY);
 const endpointSecret = process.env.SESSION_COMPLETED;
@@ -103,8 +15,8 @@ if (!endpointSecret) {
     Response.json({ message: 'Server configuration error' }, { status: 500 });
 }
 
-export async function POST(request) {
-    const body = await request.text();
+export async function POST(req, res) {
+    const body = await req.text();
 
     const headerList = await headers();
 
@@ -115,7 +27,7 @@ export async function POST(request) {
     try {
         event = stripe.webhooks.constructEvent(body, sig, endpointSecret);
     } catch (err) {
-        Response.json({ message: err.message });
+        return Response.json({ message: err.message }, { status: 500 });
     }
     // Handle the event
     switch (event.type) {
@@ -211,5 +123,5 @@ export async function POST(request) {
             console.log('Other events');
     }
 
-    Response.json({ received: true }, { status: 200 });
+    return Response.json({ received: true }, { status: 200 });
 }
